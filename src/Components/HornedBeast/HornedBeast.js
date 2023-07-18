dc689d0318b1633e8e4d4539fcdaefcde6d18469
sarahwoolsey87 <shedontusejelly0@gmail.com>
2023-07-18 17:29:56 +0100

    update

--- src/Components/HornedBeast/HornedBeast.js
+++ src/Components/HornedBeast/HornedBeast.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import './HornedBeast.css';
+import React, { useState } from "react";
+import "./HornedBeast.css";
 
-function HornedBeast({ title, imageUrl, description }) {
+export default function HornedBeast({ title, imgUrl, description }) {
+  const [fav, setFav] = useState(0);
+
+  function handleFav() {
+    setFav(fav + 1);
+  }
+
   return (
-    <div className="horned-beast">
+    <div className="hb-container">
       <h2>{title}</h2>
-      <img src={imageUrl} alt={title} title={title} />
-      <p>{description}</p>
+      <img className="hb-image" src={imgUrl} alt={title} />
+      <p className="hb-content">{description}</p>
+      <p>
+        <span className="heart" onClick={handleFav}>
+          ❤️
+        </span>
+        {fav}
+      </p>
     </div>
   );
 }
-
-export default HornedBeast;
