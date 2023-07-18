da82b8de899f26dff19203cb07198951a0eda678
sarahwoolsey87 <shedontusejelly0@gmail.com>
2023-07-18 11:39:55 +0100

    update

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './Header.css';
+import logo from './logo.jpg'; 
 
 function Header() {
-  return <h1 className="title">Beast World</h1>;
+  return (
+    <div className="header">
+      <img src={logo} alt="Logo" className="logo" />
+      <h1 className="title">Beast World</h1>
+    </div>
+  );
 }
 
 export default Header;
-
-
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -5,12 +5,12 @@ function Main() {
   const hornedBeasts = [
     {
       title: "Unicorn",
-      imageUrl: "https://example.com/unicorn.jpg",
+      imageUrl: "https://th.bing.com/th/id/OIP.5LBIhFUtNlQCojQ8tgt_pgHaEK?pid=ImgDet&rs=1",
       description: "A magical creature with a horn on its head."
     },
     {
       title: "Dragon",
-      imageUrl: "https://example.com/dragon.jpg",
+      imageUrl: "https://th.bing.com/th/id/R.302d9543cc0f2be1119b69ce64f97d0a?rik=dRclmSaGMc49pQ&riu=http%3a%2f%2fdurian.blender.org%2fwp-content%2fuploads%2f2009%2f10%2fsintel-wallpaper-dragon.jpg&ehk=0W49OzbyoFZuLe%2bW2zlRhSgZ7vWMH37q5fufMdr%2bf4M%3d&risl=&pid=ImgRaw&r=0",
       description: "A mythical creature that can breathe fire."
     }
   ];
